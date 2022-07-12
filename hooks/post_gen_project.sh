7b4de7e43061f501b401faefd4410683fa89f9ec
Tim Mironov <timirlan666@gmail.com>
2022-07-13 02:00:39 +0500

    add questions to install sentry and loguru

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -5,5 +5,14 @@ git init
 poetry install
 poetry run pre-commit install
 
+
+{% if cookiecutter.install_sentry == "y" %}
+poetry add sentry-sdk
+{% endif %}
+
+{% if cookiecutter.install_loguru == "y" %}
+poetry add loguru
+{% endif %}
+
 git add -A
 git commit -am "initial"
